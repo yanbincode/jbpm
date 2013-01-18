d69af8fcc7b2b301cce5f77f4e1d51f64f0b4122
yanbin <yb_860830@163.com>
2013-01-17 18:19:17 -0800

    jbpm持续开发

---
+++ src/main/java/service/ExpenseService.java
@@ -0,0 +1,14 @@
+package service;
+
+import model.Expense;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpenseService {
+
+	public Long jbpmExecute(Expense expense) {
+		return null;
+	}
+
+}
